e697e36573d6fa1e29580796397669d142e4b810
Bruno Oliveira <brunodevoliveira@gmail.com>
2023-04-30 20:37:06 -0300

    RXJS - curso finalizado

--- rxjs-buscante/src/app/service/livro.service.ts
+++ rxjs-buscante/src/app/service/livro.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable, tap } from 'rxjs';
-import { LivrosResultado } from '../models/interfaces';
+import { LivrosResultado, Item } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -12,9 +12,10 @@ export class LivroService {
 
   buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params: HttpParams = new HttpParams().append('q', valorDigitado);
-    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      tap(console.log), //* Não modifica os dados, serve para visualizá-los e gerar algum efeito colateral
-      map((resposta) => resposta.items)
-    );
+    return this.http.get<LivrosResultado>(this.API, { params });
+    // .pipe(
+    //   tap(console.log), //* Não modifica os dados, serve para visualizá-los e gerar algum efeito colateral
+    //   map((resposta) => resposta.items ?? [])
+    // );
   }
 }
--- rxjs-buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ rxjs-buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,23 @@
-import { Livro } from './../../models/interfaces';
+import { Item, Livro, LivrosResultado } from './../../models/interfaces';
 import { Component, inject, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  EMPTY,
+  filter,
+  map,
+  of,
+  Subscription,
+  switchMap,
+  tap,
+  throwError,
+} from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { FormControl } from '@angular/forms';
+
+const PAUSA = 1000;
 
 @Component({
   selector: 'app-lista-livros',
@@ -10,43 +26,58 @@ import { LivroService } from 'src/app/service/livro.service';
 })
 export class ListaLivrosComponent implements OnDestroy {
   public listaLivros: Livro[];
-  public campoBusca: string = '';
+  public campoBusca: FormControl = new FormControl();
+  public mensagemErro: string = '';
+  public livrosResultado: LivrosResultado;
   public subscription: Subscription;
   public livro: Livro;
+  public termoBuscado: string;
 
   private livroService = inject(LivroService);
 
-  buscarLivros() {
-    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
-      next: (value) => {
-        this.listaLivros = this.livrosResultadoParaLivros(value);
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
-  }
+  public totalDeLivros;
 
-  livrosResultadoParaLivros(items): Livro[] {
-    const livros: Livro[] = [];
+  public livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.livroService.buscar(valorDigitado)),
+    tap(({ totalItems }) => (this.totalDeLivros = totalItems)),
+    map((resposta) => resposta.items ?? []),
+    map((items) => this.livrosResultadoParaLivros(items)),
+    catchError((err) => {
+      //!capturar erros na requisição
+      // this.mensagemErro = 'Ops, ocorreu um erro! Recarregue a aplicação.';
+      // return EMPTY;
 
-    items.forEach((item) => {
-      livros.push(
-        (this.livro = {
-          title: item.volumeInfo?.title,
-          authors: item.volumeInfo?.authors,
-          publisher: item.volumeInfo?.publisher,
-          publishedDate: item.volumeInfo?.publishedDate,
-          description: item.volumeInfo?.description,
-          previewLink: item.volumeInfo?.previewLink,
-          thumbnail: item.volumeInfo?.imageLinks?.thumbnail,
-        })
+      console.log(err);
+      return throwError(
+        //! criará um Observable com a instância de erro
+        () =>
+          new Error(
+            (this.mensagemErro =
+              'Ops, ocorreu um erro! Recarregue a aplicação.')
+          )
       );
-    });
-    return livros;
+    })
+  );
+
+  // buscarLivros() {
+  //   this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
+  //     next: (value) => {
+  //       this.listaLivros = this.livrosResultadoParaLivros(value);
+  //     },
+  //     error: (err) => {
+  //       console.error(err);
+  //     },
+  //   });
+  // }
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map((item) => new LivroVolumeInfo(item));
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    // this.subscription.unsubscribe();
   }
 }
