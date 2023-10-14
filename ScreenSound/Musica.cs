2cb82c34da1ee95194c69a9b988b0c875b28fa42
Bruno Oliveira <brunodevoliveira@gmail.com>
2023-10-14 20:14:11 -0300

    Csharp - OOP

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,21 @@
+﻿class Musica
+{
+    public string Nome {  get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    
+    //GET e Set são propriedades
+    public bool Disponivel { get; set; }
+
+    //GET utilizando Lambda
+    public string DescricaoResumida => $"A música {Nome} pertence a banda {Artista}";
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        Console.WriteLine(Disponivel ? "Música disponível no plano" : "Adquira o plano Plus+");
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,16 @@
+﻿Musica musica1 = new Musica();
+musica1.Nome = "Pescador de Ilusões";
+musica1.Artista = "O Rappa";
+musica1.Duracao = 273;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.DescricaoResumida);
+
+Musica musica2 = new Musica();
+musica2.Nome = "Me deixa"; 
+musica2.Artista = "O Rappa";
+musica2.Duracao = 372;
+musica2.Disponivel = false;
+
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
